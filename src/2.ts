ba0b28a32efa2227b61c08a7a44d57dbf4a6b7f2
julietbrachtv1at <julietbrachtv1at@gmx.com>
2025-03-04 19:07:01 +0000

    Add 2.ts

---
+++ src/2.ts
@@ -0,0 +1,15 @@
+const add = (a: number, b: number) => {
+  return a + b;
+}
+
+const subtract = (a: number, b: number) => {
+  return a - b;
+}
+
+const multiply = (a: number, b: number) => {
+  return a * b;
+}
+
+const divide = (a: number, b: number) => {
+  return a / b;
+}
