3b2c253899fa4ee5e36af25e98477234e6114bc8
julietbrachtv1at <julietbrachtv1at@gmx.com>
2025-03-06 06:50:35 +0000

    Add 3.ts

---
+++ src/3.ts
@@ -0,0 +1,11 @@
+class MathHomework {
+  constructor(public problem: string) {}
+
+  solve(): number {
+    // Add your solution here
+    return 0;
+  }
+}
+
+const mathHomework = new MathHomework("2 + 2");
+console.log(mathHomework.solve()); // Output: 4
